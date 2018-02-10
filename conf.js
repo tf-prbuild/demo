1a963bc0536e40f2eb9fb3c89503310cebc8cf73
Titus Fortner <titusfortner@gmail.com>
2018-02-10 14:03:21 -0600

    bigger is better

--- conf.js
+++ conf.js
@@ -42,8 +42,8 @@ exports.config = {
     maxInstances: 25
   }],
 
-  getPageTimeout: 30000,
-  allScriptsTimeout: 30000,
+  getPageTimeout: 60000,
+  allScriptsTimeout: 60000,
 
   onComplete: function() {
 
